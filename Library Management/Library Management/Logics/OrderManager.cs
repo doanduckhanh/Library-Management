69c6e930593adb94f27bb70ab4275dd7e811c247
khanhddhe150956 <khanhddhe150956@fpt.edu.vn>
2022-07-16 09:30:07 +0700

    Update OrderManager.cs

--- Library Management/Library Management/Logics/OrderManager.cs
+++ Library Management/Library Management/Logics/OrderManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library_Management.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Library_Management.Logics
 {
-    internal class OrderManager
+    class OrderManager
     {
+        
     }
 }
