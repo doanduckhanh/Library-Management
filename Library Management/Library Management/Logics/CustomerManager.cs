86e863cb26647ed72aa4cf830f130a0757b2c794
khanhddhe150956 <khanhddhe150956@fpt.edu.vn>
2022-07-16 10:05:39 +0700

    Update CustomerManager.cs

--- Library Management/Library Management/Logics/CustomerManager.cs
+++ Library Management/Library Management/Logics/CustomerManager.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library_Management.Models;
 
 namespace Library_Management.Logics
 {
-    internal class CustomerManager
+    class CustomerManager
     {
+        LibraryDbContext db = new LibraryDbContext();
+
+        public CustomerManager()
+        {
+            LibraryDbContext db = new LibraryDbContext();
+        }
+
+        public List<Customer> GetBookList()
+        {
+            return db.Customers.ToList();
+        }
     }
 }
