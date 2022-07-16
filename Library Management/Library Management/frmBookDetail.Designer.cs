45b3689b1dd1c341f2eb0ed82b6910431cf68124
khanhddhe150956 <khanhddhe150956@fpt.edu.vn>
2022-07-16 09:52:06 +0700

    Update frmBookDetail.Designer.cs

--- Library Management/Library Management/frmBookDetail.Designer.cs
+++ Library Management/Library Management/frmBookDetail.Designer.cs
@@ -28,20 +28,249 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.tbAuthor = new System.Windows.Forms.TextBox();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            this.dtpEntryDate = new System.Windows.Forms.DateTimePicker();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.nudPrice = new System.Windows.Forms.NumericUpDown();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrice)).BeginInit();
             this.SuspendLayout();
             // 
-            // BookDetail
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(135, 38);
+            this.tbID.Name = "tbID";
+            this.tbID.ReadOnly = true;
+            this.tbID.Size = new System.Drawing.Size(125, 27);
+            this.tbID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Category";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Author";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Number ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "EntryDate";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(35, 405);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Price";
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(135, 96);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(256, 27);
+            this.tbTitle.TabIndex = 8;
+            // 
+            // tbAuthor
+            // 
+            this.tbAuthor.Location = new System.Drawing.Point(135, 214);
+            this.tbAuthor.Name = "tbAuthor";
+            this.tbAuthor.Size = new System.Drawing.Size(167, 27);
+            this.tbAuthor.TabIndex = 10;
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Location = new System.Drawing.Point(135, 288);
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(150, 27);
+            this.nudNumber.TabIndex = 11;
+            // 
+            // dtpEntryDate
+            // 
+            this.dtpEntryDate.Location = new System.Drawing.Point(135, 345);
+            this.dtpEntryDate.Name = "dtpEntryDate";
+            this.dtpEntryDate.Size = new System.Drawing.Size(250, 27);
+            this.dtpEntryDate.TabIndex = 12;
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(135, 159);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(151, 28);
+            this.cbCategory.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(292, 405);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 20);
+            this.label8.TabIndex = 15;
+            this.label8.Text = ".000 VND";
+            // 
+            // nudPrice
+            // 
+            this.nudPrice.Location = new System.Drawing.Point(136, 403);
+            this.nudPrice.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudPrice.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudPrice.Name = "nudPrice";
+            this.nudPrice.Size = new System.Drawing.Size(150, 27);
+            this.nudPrice.TabIndex = 16;
+            this.nudPrice.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.BackColor = System.Drawing.Color.FloralWhite;
+            this.btUpdate.Location = new System.Drawing.Point(166, 480);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(94, 29);
+            this.btUpdate.TabIndex = 17;
+            this.btUpdate.Text = "Update";
+            this.btUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btDelete
+            // 
+            this.btDelete.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btDelete.Location = new System.Drawing.Point(291, 480);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(94, 29);
+            this.btDelete.TabIndex = 18;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = false;
+            // 
+            // btAdd
+            // 
+            this.btAdd.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btAdd.Location = new System.Drawing.Point(33, 480);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(94, 29);
+            this.btAdd.TabIndex = 19;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = false;
+            // 
+            // frmBookDetail
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(613, 506);
-            this.Name = "BookDetail";
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(509, 566);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.btUpdate);
+            this.Controls.Add(this.nudPrice);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cbCategory);
+            this.Controls.Add(this.dtpEntryDate);
+            this.Controls.Add(this.nudNumber);
+            this.Controls.Add(this.tbAuthor);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbID);
+            this.Name = "frmBookDetail";
             this.Text = "Book Detail";
             this.Load += new System.EventHandler(this.BookDetail_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrice)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.TextBox tbAuthor;
+        private System.Windows.Forms.NumericUpDown nudNumber;
+        private System.Windows.Forms.DateTimePicker dtpEntryDate;
+        private System.Windows.Forms.ComboBox cbCategory;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown nudPrice;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button btAdd;
     }
 }
