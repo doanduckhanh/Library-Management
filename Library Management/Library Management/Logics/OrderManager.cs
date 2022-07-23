d8f50a9dde27bacbb89c75b3bda673d38a0e6e00
khanhddhe150956 <khanhddhe150956@fpt.edu.vn>
2022-07-24 01:23:31 +0700

    Update OrderManager.cs

--- Library Management/Library Management/Logics/OrderManager.cs
+++ Library Management/Library Management/Logics/OrderManager.cs
@@ -20,5 +20,30 @@ namespace Library_Management.Logics
         {
             return db.Orders.ToList();
         }
+        public void AddOrder(Order a)
+        {
+            db.Orders.Add(a);
+            db.SaveChanges();
+        }
+        public void UpdateOrder(Order a)
+        {
+            Order o = db.Orders.FirstOrDefault(x => x.OrId == a.OrId);
+            o.CusId = a.CusId;
+            o.BookId = a.BookId;
+            o.StartDat = a.StartDat;
+            o.EndDate = a.EndDate;
+            o.Status = a.Status;
+            db.SaveChanges();
+        }
+        public void DeleteOrder(Order a)
+        {
+            Order o = db.Orders.FirstOrDefault(x => x.OrId == a.OrId);
+            db.Orders.Remove(o);
+            db.SaveChanges();
+        }
+        public List<Order> GetOrderPending()
+        {
+            return db.Orders.Where(x => x.EndDate < DateTime.Now && x.Status == false).ToList();
+        }
     }
 }
