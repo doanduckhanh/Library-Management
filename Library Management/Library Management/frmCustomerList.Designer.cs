eab21f3135f0e9dc646d0b2bcee0558138812df6
khanhddhe150956 <khanhddhe150956@fpt.edu.vn>
2022-07-24 01:22:31 +0700

    Update Form Customer List

--- Library Management/Library Management/frmCustomerList.Designer.cs
+++ Library Management/Library Management/frmCustomerList.Designer.cs
@@ -30,11 +30,9 @@
         {
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.cbSortBy = new System.Windows.Forms.ComboBox();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.cbSearch = new System.Windows.Forms.ComboBox();
+            this.btSearch = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -61,46 +59,39 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Search";
             // 
-            // textBox1
+            // tbSearch
             // 
-            this.textBox1.Location = new System.Drawing.Point(179, 27);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(186, 27);
-            this.textBox1.TabIndex = 2;
+            this.tbSearch.Location = new System.Drawing.Point(179, 27);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(186, 27);
+            this.tbSearch.TabIndex = 2;
             // 
-            // comboBox1
+            // cbSearch
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(371, 27);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(123, 28);
-            this.comboBox1.TabIndex = 3;
+            this.cbSearch.FormattingEnabled = true;
+            this.cbSearch.Items.AddRange(new object[] {
+            "Id",
+            "Name",
+            "Address",
+            "State",
+            "City",
+            "Dob",
+            "Phone",
+            "Email"});
+            this.cbSearch.Location = new System.Drawing.Point(371, 27);
+            this.cbSearch.Name = "cbSearch";
+            this.cbSearch.Size = new System.Drawing.Size(123, 28);
+            this.cbSearch.TabIndex = 3;
             // 
-            // button1
+            // btSearch
             // 
-            this.button1.Location = new System.Drawing.Point(500, 27);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Search";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(812, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 20);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Sort By";
-            // 
-            // cbSortBy
-            // 
-            this.cbSortBy.FormattingEnabled = true;
-            this.cbSortBy.Location = new System.Drawing.Point(877, 28);
-            this.cbSortBy.Name = "cbSortBy";
-            this.cbSortBy.Size = new System.Drawing.Size(178, 28);
-            this.cbSortBy.TabIndex = 6;
+            this.btSearch.Location = new System.Drawing.Point(500, 27);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(94, 29);
+            this.btSearch.TabIndex = 4;
+            this.btSearch.Text = "Search";
+            this.btSearch.UseVisualStyleBackColor = true;
+            this.btSearch.Click += new System.EventHandler(this.btSearch_Click);
             // 
             // textBox2
             // 
@@ -137,11 +128,9 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.cbSortBy);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btSearch);
+            this.Controls.Add(this.cbSearch);
+            this.Controls.Add(this.tbSearch);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -159,11 +148,9 @@
 
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox cbSortBy;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.ComboBox cbSearch;
+        private System.Windows.Forms.Button btSearch;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
--- Library Management/Library Management/frmCustomerList.cs
+++ Library Management/Library Management/frmCustomerList.cs
@@ -63,5 +63,47 @@ namespace Library_Management
             CustomerManager customerManager = new CustomerManager();
             dataGridView2.DataSource = customerManager.GetCusList();
         }
+        private class RowComparer : System.Collections.IComparer
+        {
+            private static int sortOrderModifier = 1;
+
+            public RowComparer(SortOrder sortOrder)
+            {
+                if (sortOrder == SortOrder.Descending)
+                {
+                    sortOrderModifier = -1;
+                }
+                else if (sortOrder == SortOrder.Ascending)
+                {
+                    sortOrderModifier = 1;
+                }
+            }
+
+            public int Compare(object x, object y)
+            {
+                DataGridViewRow DataGridViewRow1 = (DataGridViewRow)x;
+                DataGridViewRow DataGridViewRow2 = (DataGridViewRow)y;
+
+                // Try to sort based on the Last Name column.
+                int CompareResult = System.String.Compare(
+                    DataGridViewRow1.Cells[1].Value.ToString(),
+                    DataGridViewRow2.Cells[1].Value.ToString());
+
+                // If the Last Names are equal, sort based on the First Name.
+                if (CompareResult == 0)
+                {
+                    CompareResult = System.String.Compare(
+                        DataGridViewRow1.Cells[0].Value.ToString(),
+                        DataGridViewRow2.Cells[0].Value.ToString());
+                }
+                return CompareResult * sortOrderModifier;
+            }
+        }
+
+        private void btSearch_Click(object sender, EventArgs e)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            dataGridView2.DataSource = customerManager.SearchCustomer(cbSearch.Text, tbSearch.Text);
+        }
     }
 }
