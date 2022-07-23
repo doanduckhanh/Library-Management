98745f8ac8860d51875af93f1573ce4ddc45ae2c
khanhddhe150956 <khanhddhe150956@fpt.edu.vn>
2022-07-24 01:22:45 +0700

    Add Form Import

---
+++ Library Management/Library Management/frmImport.cs
@@ -0,0 +1,56 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_Management
+{
+    public partial class frmImport : Form
+    {
+        DataSet ds;
+        public frmImport()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Workbook|*.xlsx|Excel Workbook 97-2003|*.xls", ValidateNames = true })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using (var stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        IExcelDataReader reader;
+                        if (ofd.FilterIndex == 2)
+                        {
+                            reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                        }
+                        else
+                        {
+                            reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                        }
+
+                        ds = reader.AsDataSet(new ExcelDataSetConfiguration()
+                        {
+                            ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                            {
+                                UseHeaderRow = true
+                            }
+                        });
+
+                    }
+                }
+            }
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+    }
+}
